a2504622cda1f6cbdb0100fa2bc8d44b3aeae8c7
Alexandros Fragopoulos <081520@tutanota.com>
2022-01-18 18:20:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
+## Password Manager
 
-
-import re, getpass, os, csv
-
+import re, os, csv
+from getpass import getpass
 
 ## First part 
 
-print("1. New user /n2. Login ")
+print("1. New user \n2. Login ")
 
-before_login = input("Select a number (from 1 to 2): ")
+before_login = int(input("Select a number (from 1 to 2): "))
 
 class new_user():
     
-    def __init__(self, new_username, master_password, rep_passwd ):
+    def __init__(self, new_username, master_password, rep_password ):
         
         self.new_username = new_username
         self.master_password = master_password 
+        self.rep_password = rep_password
 
-    def password_verification(self,master_password, rep_passwd):
+    def password_verification(self, master_password, rep_password):
         
-        if (self.master_password = self.rep_passwd):
+        if (self.master_password == self.rep_password):
             pass 
         else:
             print("The master password is not the same with repeat password")
 
-new_user1 = new_user(input("Enter your username:"), )  
+
+if before_login == 1:
+
+    new_username_input = input("Enter your username: ")
+    new_master_password_input = getpass("Enter your master password: ")
+    new_rep_password_input = getpass("Repeat your master password: ")
+
+    new_user1 = (new_username_input, new_master_password_input, new_rep_password_input)
+    
+    new_user1.password_verification(new_user1.new_master_password_input, new_user1.new_rep_password_input)
+
 
 ## Second part 
 
-print("This my password manager \n \n    1. Add new acount \n    2. See details of account ")
+print("\n \n    1. Add new acount \n    2. See details of account ")
 
 after_login = str(input("Select a number (from 1 to 2): "))
 
