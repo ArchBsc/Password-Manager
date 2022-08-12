588f39d0008c544fd23a084f301c2b3b41a7e58f
ArchBsc <081520@tutanota.com>
2022-08-12 21:20:43 +0300

    Commit: 12-08-22 21:20 v1.41

--- installer.py
+++
@@ -1,6 +0,0 @@
-import subprocess, os
-
-def run():
-    print("To run the program you need to has python installed and pip to work")
-    os.system("pip install hashlib cryptography rich tabulate")
-run()
---
+++ installer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pip install hashlib cryptography rich tabulate
